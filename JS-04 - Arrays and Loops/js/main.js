c9fd9868c27270d27b8fb11f9e06eb7422e145b2
doraluz <doraluz.gzatarain@gmail.com>
2023-03-27 17:23:03 -0600

    Exercise 2

--- JS-04 - Arrays and Loops/js/main.js
+++ JS-04 - Arrays and Loops/js/main.js
@@ -1,4 +1,5 @@
 let divName = document.getElementById("divName");
+let divTwo = document.getElementById("divTwo");
 
 // EXERCISE 1
 let names = ["Sofia", "David", "Juan"];
@@ -10,3 +11,12 @@ names.push("Elena");
 let strName = names.join(", ");
 divName.innerHTML = strName;
 
+// EXERCISE 2
+let element = ["*"];
+console.log(element);
+for (let start=1 ; start<=5 ; start++){
+    for( let x=1 ; x<=start; x++){
+        divTwo.innerHTML += element ;
+    }
+    divTwo.innerHTML += "<br>";
+}
