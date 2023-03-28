0e6055c751d7be5f7362a111b35783b12d65d416
doraluz <doraluz.gzatarain@gmail.com>
2023-03-27 19:31:34 -0600

    Complete Exercise 3

--- JS-04 - Arrays and Loops/js/main.js
+++ JS-04 - Arrays and Loops/js/main.js
@@ -1,5 +1,10 @@
 let divName = document.getElementById("divName");
 let divTwo = document.getElementById("divTwo");
+let divThree = document.getElementById("divThree");
+let divFour = document.getElementById("divFour");
+let divFive = document.getElementById("divFive");
+let divSix = document.getElementById("divSix");
+
 
 // EXERCISE 1
 let names = ["Sofia", "David", "Juan"];
@@ -20,3 +25,44 @@ for (let start=1 ; start<=5 ; start++){
     }
     divTwo.innerHTML += "<br>";
 }
+
+//EXERCISE 3
+let xValue = 10;
+divThree.innerHTML = "Repeatedly print the value of the variable xValue, decreasing it by 0.5 each time, as long as xValue remains positive." + "<br>";
+while(xValue > 0){
+    console.log(xValue);
+    divThree.innerHTML += xValue + "<br>";
+    xValue -= 0.5;
+}
+
+divFour.innerHTML = "Print all the odd numbers between 1 - 100." + "<br>";
+xValue = 1;
+while (xValue < 100){
+    if (xValue !=1){
+        console.log(xValue);
+        divFour.innerHTML += xValue + "<br>";
+    }
+    xValue++;
+}
+
+divFive.innerHTML = "Write a method with a while loop to print 1 through n in square brackets." + "<br>";
+let n = 8;
+let s = 1;
+while (s <=n){
+    console.log(s);
+    divFive.innerHTML += "[" + s + "]" + "<br>" ;
+    s++;
+}
+
+s = 0;
+let i = 1;
+let d = 19;
+function sum (s,i){
+    while(i <= d){
+        s = i + s;
+        i++;
+    }
+    return s;
+} 
+console.log(sum(s,i));
+divSix.innerHTML = "Write a method with a while loop that computes the sum of first n positive integers:" + "<br>" + "n = " + d + "<br>" + "Result = " + sum(s,i);
